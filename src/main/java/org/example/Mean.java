0b99468c414843cf105b91fa6ea43a0218fb6bd8
Atanas Bakalov <atanas.bakalov@telenordigital.com>
2022-03-15 06:57:05 +0100

    docs

--- src/main/java/org/example/Mean.java
+++ src/main/java/org/example/Mean.java
@@ -1,6 +1,14 @@
 package org.example;
 
+/**
+ * Class suitable for calculating mean value of different slices of a constant array multiple times
+ */
 public class Mean {
+    /**
+     * Constructor
+     * @param elements The constant array
+     * @throws IllegalArgumentException if array is null or empty
+     */
     public Mean(final int[] elements) {
         if (elements == null || elements.length == 0) {
             throw new IllegalArgumentException("empty list");
@@ -12,6 +20,13 @@ public class Mean {
         }
     }
 
+    /**
+     * Gets the mean value of the slice
+     * @param l Left boundary of the slice
+     * @param r Right boundary of the slice
+     * @return The mean value
+     * @throws IllegalArgumentException in case of out of boundary slice indexes
+     */
     public double getMean(int l, int r) {
         if (!((l >= 0) && (r >= 0) && (l <= r ) && (l < preCalcSums.length) && (r < preCalcSums.length))) {
             throw new IllegalArgumentException("illegal slice");
